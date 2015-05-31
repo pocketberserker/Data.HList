2dfe4ddb05e85ed42a7929994ff9106a05d3872a
pocketberserker <pocketberserker@gmail.com>
2015-06-01 04:47:17 +0900

    fix typo...

--- FSharp.Data.HList/AssemblyInfo.fs
+++ FSharp.Data.HList/AssemblyInfo.fs
@@ -3,8 +3,8 @@
 open System.Reflection
 open System.Runtime.InteropServices
 
-[<assembly: AssemblyTitle("FSharp.Data.List")>]
-[<assembly: AssemblyProduct("FSharp.Data.List")>]
+[<assembly: AssemblyTitle("FSharp.Data.HList")>]
+[<assembly: AssemblyProduct("FSharp.Data.HList")>]
 [<assembly: Guid("C5977F56-636F-4BA5-8424-B337E4E677B7")>]
 
 [<assembly: AssemblyVersion("0.1.0.0")>]
