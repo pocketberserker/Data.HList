3e8af2a839bde14139f6932cb64242e97cc92536
pocketberserker <pocketberserker@gmail.com>
2015-06-01 22:49:52 +0900

    0.1.2

--- FSharp.Data.HList/AssemblyInfo.fs
+++ FSharp.Data.HList/AssemblyInfo.fs
@@ -7,7 +7,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyProduct("FSharp.Data.HList")>]
 [<assembly: Guid("C5977F56-636F-4BA5-8424-B337E4E677B7")>]
 
-[<assembly: AssemblyVersion("0.1.1.0")>]
-[<assembly: AssemblyFileVersion("0.1.1.0")>]
+[<assembly: AssemblyVersion("0.1.2.0")>]
+[<assembly: AssemblyFileVersion("0.1.2.0")>]
 
 do ()
