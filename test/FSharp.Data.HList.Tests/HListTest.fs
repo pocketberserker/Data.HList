09a1098e7069d5d0401967b5d275e2e8cdeb06a9
pocketberserker <pocketberserker@gmail.com>
2016-11-03 16:54:44 +0900

    fix namespace

--- test/FSharp.Data.HList.Tests/HListTest.fs
+++ test/FSharp.Data.HList.Tests/HListTest.fs
@@ -1,51 +1,48 @@
-﻿namespace FSharp.Data.HList.Tests
+﻿module FSharp.Data.Tests.HListTest
 
 open Persimmon
 open UseTestNameByReflection
 open FSharp.Data
 
-module HListTest =
+let ``head test`` = test {
+  let a = HList.nil |> HList.cons true |> HList.cons 3 |> HList.cons "Foo"
+  do! assertEquals "Foo" (a |> HList.head)
+  return a
+}
 
-  let ``head test`` = test {
-    let a = HList.nil |> HList.cons true |> HList.cons 3 |> HList.cons "Foo"
-    do! assertEquals "Foo" (a |> HList.head)
-    return a
-  }
+let ``tail test`` = test {
+  let a = HList.nil |> HList.cons [1; 2] |> HList.cons "Bar" |> HList.cons 4.0
+  do! assertEquals [1; 2] (a |> HList.tail |> HList.tail |> HList.head)
+  return a
+}
 
-  let ``tail test`` = test {
-    let a = HList.nil |> HList.cons [1; 2] |> HList.cons "Bar" |> HList.cons 4.0
-    do! assertEquals [1; 2] (a |> HList.tail |> HList.tail |> HList.head)
-    return a
-  }
+let ``append test`` = test {
+  let! a = ``head test``
+  let! b = ``tail test``
+  let zero = HAppend.define
+  let one = HAppend.append zero
+  let two = HAppend.append one
+  let three = HAppend.append two
+  let x = HList.append three a b
+  do! assertEquals "Foo" (HList.head x)
+  do! assertEquals "Bar" (x |> HList.tail |> HList.tail |> HList.tail |> HList.tail |> HList.head)
+  return x
+}
 
-  let ``append test`` = test {
-    let! a = ``head test``
-    let! b = ``tail test``
-    let zero = HAppend.define
-    let one = HAppend.append zero
-    let two = HAppend.append one
-    let three = HAppend.append two
-    let x = HList.append three a b
-    do! assertEquals "Foo" (HList.head x)
-    do! assertEquals "Bar" (x |> HList.tail |> HList.tail |> HList.tail |> HList.tail |> HList.head)
-    return x
-  }
+let ``length test`` = test {
+  let! a = ``head test``
+  let! b = ``tail test``
+  let! x = ``append test``
+  do! assertEquals 0 (HList.length HList.nil)
+  do! assertEquals 3 (HList.length a)
+  do! assertEquals (HList.length a + HList.length b) (HList.length x)
+}
 
-  let ``length test`` = test {
-    let! a = ``head test``
-    let! b = ``tail test``
-    let! x = ``append test``
-    do! assertEquals 0 (HList.length HList.nil)
-    do! assertEquals 3 (HList.length a)
-    do! assertEquals (HList.length a + HList.length b) (HList.length x)
-  }
-
-  let ``foldBack test`` = test {
-    let functions = HList.singleton (fun x -> 1 + x) |> HList.cons (fun x -> 2 * x) |> HList.cons String.length
-    let comp1 = Apply.comp
-    let comp0 = Apply.comp
-    let fold0 = HFoldBack.define
-    let fold2 = HFoldBack.foldBack comp1 (HFoldBack.foldBack comp0 (HFoldBack.foldBack comp0 fold0))
-    do! assertEquals 7 (HList.foldBack fold2 () id functions "abc")
-  }
-
+let ``foldBack test`` = test {
+  let functions = HList.singleton (fun x -> 1 + x) |> HList.cons (fun x -> 2 * x) |> HList.cons String.length
+  let comp1 = Apply.comp
+  let comp0 = Apply.comp
+  let fold0 = HFoldBack.define
+  let fold2 = HFoldBack.foldBack comp1 (HFoldBack.foldBack comp0 (HFoldBack.foldBack comp0 fold0))
+  do! assertEquals 7 (HList.foldBack fold2 () id functions "abc")
+}
--- test/FSharp.Data.HList.Tests/HPreTest.fs
+++ test/FSharp.Data.HList.Tests/HPreTest.fs
@@ -1,12 +1,11 @@
-﻿namespace FSharp.Data.HPre.Tests
+﻿module FSharp.Data.Tests.HPreTest
 
 open Persimmon
 open UseTestNameByReflection
 open FSharp.Data.HPre
 
-module HPreTest =
 
-  let ``a + 0 = a`` = test {
-    let a = HNat.hsucc(HNat.hsucc(HNat.hsucc(HNat.hzero)))
-    do! assertEquals (HNat.toInt a) ((HAdd.add(a, HNat.hzero)).Sum |> HNat.toInt)
-  }
+let ``a + 0 = a`` = test {
+  let a = HNat.hsucc(HNat.hsucc(HNat.hsucc(HNat.hzero)))
+  do! assertEquals (HNat.toInt a) ((HAdd.add(a, HNat.hzero)).Sum |> HNat.toInt)
+}
